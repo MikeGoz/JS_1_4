a7bd67c7fe4f9eb2456f83b0f736ed2d3e2affa0
Mike <gozdz@onet.pl>
2017-09-11 21:18:18 +0200

    Update program.js

--- program.js
+++ program.js
@@ -5,6 +5,7 @@ var OSinfo = require('./modules/osinfo');
 process.stdin.setEncoding('utf-8');
 process.stdin.on('readable', function() {
   var input = process.stdin.read();
+  
   if (input !== null) {
     var instruction = input.toString().trim();
     var version = process.versions.node;
